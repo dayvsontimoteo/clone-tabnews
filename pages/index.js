cca198e4732be82849e4e0bce8d1601484a4de56
Dayvson Timoteo <dayvsonn.ti@gmail.com>
2025-10-23 18:11:48 -0300

    adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return <h1>
+        Se não fosse você e nossa princesa 
+        em minha vida, meu mundo seria um 
+        lugar triste e vazio.
+    </h1>;
+}
+
+export default Home;

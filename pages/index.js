f51e1eb7037d271387aca829541114b017eaf57c
Dayvson Timoteo <dayvsonn.ti@gmail.com>
2025-10-23 19:30:55 -0300

    Atualização da mensagem.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return <h1>
         Se não fosse você e nossa princesa 
         em minha vida, meu mundo seria um 
-        lugar triste e vazio.
+        lugar triste e vazio 🥰.
     </h1>;
 }
 
